18049851850efd11e96c5a705bb5dd85eee3b088
Raphael Rocha <rapharocha89@gmail.com>
2022-06-03 21:55:13 -0300

    Readme

---
+++ src/components/AddArea/index.tsx
@@ -0,0 +1,33 @@
+import { keyboardKey } from "@testing-library/user-event";
+import { useState, KeyboardEvent } from "react";
+import * as C from "./styles";
+
+type Props = {
+  onEnter: (taskName: string) => void;
+};
+
+export const AddArea = ({ onEnter }: Props) => {
+  const [inputText, setInputText] = useState("");
+
+  const handleKeyUp = (e: KeyboardEvent) => {
+    if (e.code === "Enter" && inputText !== "") {
+      onEnter(inputText);
+      setInputText("");
+    }
+  };
+  return (
+    <C.Container>
+      <div className="image">
+        {" "}
+        <p>+</p>{" "}
+      </div>
+      <input
+        type="text"
+        placeholder="Adiciona uma tarefa"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        onKeyUp={handleKeyUp}
+      />
+    </C.Container>
+  );
+};
---
+++ src/components/AddArea/styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+border: 1px solid #555;
+border-radius: 15px;
+padding:10px;
+margin:20px 0;
+display: flex;
+alig-items:center;
+p {padding:0px;
+margin:0px;
+font-size:40px;
+}
+
+.image {
+margin-right:5px;
+
+}
+
+input {
+padding:0px;
+background:transparent;
+border:0px;
+outline: 0;
+color:#FFF;
+font-size:18px;
+flex:1;
+}
+
+`;
